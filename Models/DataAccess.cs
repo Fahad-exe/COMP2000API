ddd361116ae97ad50b34dcf43e56cd5f967bf649
s1atkinson <s1atkinson@DESK-B-P0-11>
2021-11-07 11:29:04 +0000

    Add project files.

---
+++ Models/DataAccess.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace COMP2000API.Models
+{
+    public class DataAccess : DbContext
+    {
+        private readonly string _connection;
+
+        public DataAccess(IConfiguration configuration)
+        {
+            _connection = configuration.GetConnectionString("COMP2001_DB");
+        }
+
+        public bool Create(StudentProject sp)
+        {
+            using (SqlConnection sql = new SqlConnection(_connection))
+            {
+                using (SqlCommand cmd = new SqlCommand("CreateStudentProject", sql))
+                {
+                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                    cmd.Parameters.Add(new SqlParameter("@StudentID", sp.StudentID));
+                    cmd.Parameters.Add(new SqlParameter("@fName",sp.First_Name));
+                    cmd.Parameters.Add(new SqlParameter("@lName", sp.Second_Name));
+                    cmd.Parameters.Add(new SqlParameter("@Title", sp.Title));
+                    cmd.Parameters.Add(new SqlParameter("@Description", sp.Description));
+                    cmd.Parameters.Add(new SqlParameter("@Year", sp.Year));
+                }
+    }
+}
---
+++ Models/StudentProject.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace COMP2000API.Models
+{
+    public class StudentProject
+    {
+        public int ProjectID { get; set; }
+        public int StudentID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int Year { get; set; }
+        public string ThumbnailURL { get; set; }
+        public string PosterURL { get; set; }
+        public string First_Name { get; set; }
+        public string Second_Name { get; set; }
+        public string PictureURL { get; set; }
+    }
+}
